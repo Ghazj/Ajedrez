84598fbba29ab1f873d9d6cf66c9a8ee2391b5e0
Marquett <Marquett@Marquett-PC>
2019-10-07 10:56:13 -0300

    Proyecto Ajerdrez

--- Ajedrez/sources/ajedrez/MainAjedrez.java
+++ Ajedrez/sources/ajedrez/MainAjedrez.java
@@ -7,9 +7,32 @@ public class MainAjedrez {
 
 		Tablero tab1 = new Tablero();
 		
-		Casilla a1 = new Casilla();
+		String[][] tablero = new String[8][8];
 		
+for (int i = 0;i<tablero.length;i++) {
+			
+			for (int j = 0;j<tablero[i].length;j++) {
+				
+				if((i+j)%2==0) {
+					tablero[i][j] = "Negra";
+				}else {
+					tablero[i][j] = "Blanca";
+				}
+			}
+		}
 		
+for (int i = 0;i<tablero.length;i++) {
+	
+	for (int j = 0;j<tablero[i].length;j++) {
+		
+		System.out.print(tablero[i][j] + i+","+j+"|");
+		
+		}
+	System.out.println("");
+		}
+	
+}
+
 	}
 
-}
+
--- Ajedrez/sources/ajedrez/Tablero.java
+++ Ajedrez/sources/ajedrez/Tablero.java
@@ -1,18 +1,32 @@
 package ajedrez;
-
+// Par = negras Impar = blancas (casillas) (Índice de la matríz)
 public class Tablero {
 
 	private final Casilla tablero[][] = new Casilla[8][8];
 
+	public Tablero() {
+		
+	}
 	
 	public void iniciarTablero() {
 		
+		for (int i = 0;i<tablero.length;i++) {
+			
+			for (int j = 0;j<tablero[i].length;j++) {
+				
+				if((i+j)%2==0) {
+					tablero[i][j] = new CasillaNegra();//hija de casilla(crear)
+				}else {
+					tablero[i][j] = new CasillaBlanca();//hija de casilla(crear)
+				}
+			}
+		}
 		
 		}
 		
-		public String toString() {
-			return tablero[]
-		}
+	public String toString() {
+		return "";
+	
 	}
 	
 	
